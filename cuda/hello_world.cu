a18d959d0fb26a335c0386e3a5866da8d56f079b
Anthony G <givansanthony702@gmail.com>
2024-04-14 21:58:03 -0400

    cpp and cuda additions

---
+++ cuda/hello_world.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+__global__ void hello(){
+    printf("Hello World from GPU!\n");
+}
+
+// __global__ indicates that the function runs on the GPU
+
+int main(int argc, char **argv) {
+    hello<<<1,1>>>(); // <<<M, T>>> - M represents the number of blocks and T represents the number of threads in each block
+    cudaDeviceSynchronize();
+    return 0;
+}
